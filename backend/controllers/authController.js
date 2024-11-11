0644f1ae5a2004ff00693cc056be97e0dc304394
chapaedb <chapaeresso1@gmail.com>
2024-11-11 20:00:51 +0300

    Implemented login and logout

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -20,20 +20,39 @@ const registerUser =async(req, res)=>{
         await user.save();
 
         const token = createToken(user._id);
+            res.status(200).json({_id: user._id, email, token});
+        } catch (error) {
+            console.log(error);
+            res.status(500).json(error);
+        }
+    }
+
+
+const loginUser = async (req, res)=>{
+    try{
+        const {email, passwordHash} = req.body;
+        let user = await userModel.findOne({email});
+        if(!user) return res.status(400).json("User not found");
+        const isMatch = await bcrypt.compare(passwordHash, user.passwordHash);
+        if(!isMatch) return res.status(400).json("Invalid credentials");
+        const token = createToken(user._id);
         res.status(200).json({_id: user._id, email, token});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+}
+
+
+const logoutUser = (req, res)=>{
+    try{
+        res.clearCookie("token");
+        res.status(200).json("Logged out successfully")
     }
     catch(error){
         console.log(error)
         res.status(500).json(error)
     }
-};
-
-const loginUser = (req, res)=>{
-
-}
-
-const logoutUser = (req, res)=>{
-
 }
 
 module.exports = {registerUser,loginUser, logoutUser}

3ddd42e2e801ff4797675f68e50700ee33f12858
NathanielBirhanu <nbirhanu98@gmail.com>
2024-11-11 16:41:50 +0300

    register server-side logic

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,32 @@
+const userModel = require("../models/userModel")
+const bcrypt = require("bcrypt")
+const validator = require("validator")
+const registerUser =async(req, res)=>{
+    try{
+        const {email, passwordHash} = req.body;
+        let user = await userModel.findOne({email});
 
+        if(user) return res.status(400).json("user with the given email already exists");
+        if(!email || !passwordHash) return res.status(400).json("All fields are required");
+        if (!validator.isEmail(email)) return res.status(400).json("Email must be valid");
+        if (!validator.isStrongPassword(passwordHash)) return res.status(400).json("Password must be strong ");
 
-const registerUser =(req, res)=>{
-}
+        user = new userModel({email, passwordHash});
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(user.passwordHash, salt);
+
+        await user.save();
+
+        const token = createToken(user._id);
+        res.status(200).json({_id: user._id, email, token});
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json(error)
+    }
+};
 
 const loginUser = (req, res)=>{
 
